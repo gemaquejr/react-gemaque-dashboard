1f3666ce8f123da68ebefc3a93540c680144f61e
Francisco Gemaque <gemaquejr@hotmail.com>
2025-12-01 18:19:08 -0300

    feat: criar Topbar com toggle de tema e configurar estrutura base do App
    Signed-off-by: Francisco Gemaque <gemaquejr@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-function App() {
-  return (
-    <div className="app">
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Dashboard from "./scenes/dashboard";
 
-    </div>
+function App() {
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
